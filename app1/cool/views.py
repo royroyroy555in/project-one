12c413636da88da48617cc86e1547f84b7065d01
royroyroy555in <royroyroy555in@rediffmail.com>
2022-02-10 08:42:23 +0530

    commit

---
+++ app1/cool/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+TEMPLATE_DIRS = (
+    'os.path.join(BASE_DIR, "templates"),'
+)
+def index(request):
+    return render(request, "index.html")
